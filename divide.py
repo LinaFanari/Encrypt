e13c8fa17d35dd64750c04ab241b14a1a6a1d976
LinaFanari <165521413+LinaFanari@users.noreply.github.com>
2025-12-16 02:24:06 +0300

    Add files via upload

---
+++ divide.py
@@ -0,0 +1,27 @@
+def Encrypt(plantext, key):
+    Ctext=" "
+    dictt={"a":0, "b":1, "c":2, "d":3, "e":4, "f":5, "g":6, "h":7, "i":8,"j":9,
+           "k":10, "l":11, "m":12, "n":13, "o":14, "p":15, "q":16, "r":17, "s":18, "t":19,
+           "u":20, "v":21, "w":22, "x":23, "y":24, "z":25
+           }
+    for x in plantext :
+        c = (dictt[x] * key) %26
+        for v,k in dictt.items():
+            if c == k :
+                Ctext = (Ctext + v).upper()
+    print(Ctext)
+
+
+def multiCd(ciphertext, key, n):
+    ciphertext = ciphertext.lower()
+    Ptext= " "
+    dictt={"a":0, "b":1, "c":2, "d":3, "e":4, "f":5, "g":6, "h":7, "i":8,"j":9,
+           "k":10, "l":11, "m":12, "n":13, "o":14, "p":15, "q":16, "r":17, "s":18, "t":19,
+           "u":20, "v":21, "w":22, "x":23, "y":24, "z":25
+           }
+    for x in ciphertext:
+        p = (dictt[x] * BEucliedes(key, n)) %n
+        for v,k in dictt.items():
+            if p == k :
+                Ptext = (Ptext + v)
+    print(Ptext)
